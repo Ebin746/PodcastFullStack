6325c825363a702d831ebb20dac9d8715b31b47c
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2024-07-17 13:19:27 +0530

    middleware file impletemented

---
+++ backend/controllers/multer.js
@@ -0,0 +1,19 @@
+const fileUpload =async (req, res, next) => {
+    try {
+      if(!req.file){ 
+        return res.status(500).json({message:"no file found",req})
+      }
+      console.log(req.file);
+      const { filename, path, originalname } = req.file;
+      const data = new uploadSchema({
+        filename: filename,
+        path: path,
+        originalname: originalname,
+      });
+      await data.save();
+      res.status(200).json({ message: "The file has been stored" });
+    } catch (error) {
+      next(error);
+    }
+  }
+  module.exports={fileUpload}
