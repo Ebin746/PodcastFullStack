981614a9b120edfb5888d9efbbf33b691954a181
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2025-01-06 00:48:13 +0530

    refactor: remove unused file storage schema for audio uploads

--- backend/Schema/upload.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const fileStorage = new mongoose.Schema({
-  filename: String,
-  path: String,
-  originalname: String,
-});
-
-module.exports = mongoose.model("audio", fileStorage);
